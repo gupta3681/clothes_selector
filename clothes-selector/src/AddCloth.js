9ce21113a4d8c32f7bb5a2080afd9fa2ee3fd63c
Aryan Gupta <aryansep018@gmail.com>
2023-06-27 16:38:55 -0400

    fix: added mui ui
    added mui ui for buttons and texts

--- clothes-selector/src/AddCloth.js
+++ clothes-selector/src/AddCloth.js
@@ -1,41 +1,48 @@
 import React, { useState } from 'react';
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc } from 'firebase/firestore';
 import { db } from './firebase';
-import { Link } from 'react-router-dom';
+import { Button, Container, TextField, Typography } from '@mui/material';
 
-function AddCloth() {
-  const [imageURL, setImageURL] = useState('');
+function AddClothes() {
   const [description, setDescription] = useState('');
+  const [imageURL, setImageURL] = useState('');
 
-  async function onSubmit(e) {
-    e.preventDefault();
-
-    await addDoc(collection(db, 'clothes'), {
+  const addCloth = async () => {
+    const newCloth = {
+      description,
       imageURL,
-      description
-    });
+    };
 
-    setImageURL('');
+    await addDoc(collection(db, 'clothes'), newCloth);
+
     setDescription('');
-  }
+    setImageURL('');
+  };
 
   return (
-    <form onSubmit={onSubmit}>
-      <h4>Add New Cloth</h4>
-      <div>
-        <label>Image URL</label>
-        <input type="text" value={imageURL} onChange={e => setImageURL(e.currentTarget.value)} />
-      </div>
-      <div>
-        <label>Description</label>
-        <textarea value={description} onChange={e => setDescription(e.currentTarget.value)} />
-      </div>
-      <button>Add</button>
-      <Link to="/add">
-        <button>Clothes List</button>
-      </Link>
-    </form>
-  )
+    <Container>
+      <Typography variant="h2" align="center" gutterBottom>Add Clothes</Typography>
+      <form noValidate autoComplete="off">
+        <TextField 
+          label="Description"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />
+        <TextField 
+          label="Image URL"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          value={imageURL}
+          onChange={e => setImageURL(e.target.value)}
+        />
+        <Button variant="contained" color="primary" onClick={addCloth}>Add</Button>
+      </form>
+    </Container>
+  );
 }
 
-export default AddCloth;
+export default AddClothes;
--- clothes-selector/src/ClothesList.js
+++ clothes-selector/src/ClothesList.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
 import { db } from './firebase';
-import { Link } from 'react-router-dom';
-
-import './ClothesList.css';
-
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Card, CardActions, CardContent, CardMedia, Container, Typography } from '@mui/material';
 
 function ClothesList() {
   const [clothes, setClothes] = useState([]);
@@ -28,22 +26,30 @@ function ClothesList() {
   };
 
   return (
-    <div>
-      <h2 className='heading'>Clothes</h2>
-      <Link to="/add">
-        <button>Add Clothes</button>
-      </Link>
-      <div className='clothes-container'>
-      {clothes.map((cloth) => 
-        <div className='cloth-card' key={cloth.id}>
-          <img src={cloth.imageURL} alt={cloth.description} />
-          <p>{cloth.description}</p>
-          <button onClick={() => deleteCloth(cloth.id)}>Delete</button>
-        </div>
-      )}
+    <Container>
+      <Typography variant="h2" align="center" gutterBottom>Clothes</Typography>
+      <Button component={RouterLink} to="/add" variant="contained" color="primary">Add Clothes</Button>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', marginTop: '1em' }}>
+        {clothes.map((cloth) =>
+          <Card key={cloth.id} style={{ maxWidth: 345, margin: '1em' }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={cloth.imageURL}
+              alt={cloth.description}
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {cloth.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" color="secondary" onClick={() => deleteCloth(cloth.id)}>Delete</Button>
+            </CardActions>
+          </Card>
+        )}
       </div>
-      
-    </div>
+    </Container>
   )
 }
 
